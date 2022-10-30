48dd4af37829532987dbf00722b1c21c96e8fbf7
Deepro Nath <56949195+muad-dib-777@users.noreply.github.com>
2022-10-31 00:05:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+var buttons = document.querySelectorAll(".drum");
+
+
+
+for(var i = 0; i < buttons.length; i++){
+
+    buttons[i].addEventListener("click", function(){
+
+        // var audio = new Audio('sounds/tom-1.mp3');
+        // audio.play();
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+        
+        
+        // alert(buttons[i].classList)
+    })
+
+    
+}
+
+
+document.addEventListener("keydown", function(event){
+
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+
+function makeSound(key){
+
+
+    switch(key){
+
+        case "w":
+            var tom_1 = new Audio('sounds/tom-1.mp3');
+            tom_1.play();
+            break;
+        case "a":
+            var tom_2 = new Audio('sounds/tom-2.mp3');
+            tom_2.play();
+            break;
+        case "s":
+            var tom_3 = new Audio('sounds/tom-3.mp3');
+            tom_3.play();
+            break;
+        case "d":
+            var tom_4 = new Audio('sounds/tom-4.mp3');
+            tom_4.play();
+            break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            console.log(buttonInnerHTML)
+
+    }
+
+
+}
+
+
+function buttonAnimation(key){
+
+
+    var activeButton = document.querySelector(`.${key}`)
+    activeButton.classList.add('pressed');
+
+    setTimeout(function(){
+
+        activeButton.classList.remove('pressed');
+    }, 100);
+
+
+}
+
+
